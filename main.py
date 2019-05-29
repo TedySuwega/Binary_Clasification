c7569f894c5ee8bc6058de73e87fdc9b8c20d88b
Tedy Suwega <tedysuwega@gmail.com>
2019-05-29 13:16:14 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+from sklearn.model_selection import cross_validate,train_test_split
+import pandas as pd
+
+#read Data
+data = pd.read_csv('D:\Another\BinaryClasification\Binary_Clasification\Data\creditcard.csv')
+print("heloo world")
+print(data.head())
